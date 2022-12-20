d8b4128c0be50a6b09065c54052b82fdb22e59a7
Harshvardhan21052 <101208390+Harshvardhan21052@users.noreply.github.com>
2022-12-20 17:05:33 +0530

    Add files via upload

---
+++ APfinalGame-main/TankStars/TankStars/core/src/com/mygdx/tankstars/screens/Tank2Wins.java
@@ -0,0 +1,66 @@
+package com.mygdx.tankstars.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.tankstars.tankstars;
+
+public class Tank2Wins implements Screen {
+    Texture img;
+    private final tankstars game;
+    private OrthographicCamera camera;
+    public Tank2Wins(tankstars game){
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1238, 500);
+        game.batch = new SpriteBatch();
+        img = new Texture("win2.png");
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.6f, 0.46f, 0.32f, 0);
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(img,0,0);
+        game.batch.end();
+        if(Gdx.input.justTouched()){
+            HomePageGUI home1 = new HomePageGUI(game);
+            game.setScreen(home1);
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
